9074e9273f08122b804aef8ebe3e8dffa8b0d7e1
Ivan Kravchenko <ikravchenko@minint-sk42v1n.stacobon.dd13.mydorma.com>
2014-07-21 17:26:21 +0200

    Added person rows generation

--- src/main/java/org/example/MinimalServer.java
+++ src/main/java/org/example/MinimalServer.java
@@ -1,5 +1,7 @@
 package org.example;
 import java.io.IOException;
+import java.util.Random;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,23 @@ import org.eclipse.jetty.servlet.ServletHandler;
  
 public class MinimalServer {
  
+	private static final String PERSONS_RESPONSE;
+	static {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?><Persons>");
+		for (int i = 1; i <= 1000; i++) {
+			sb
+			.append("<Person number=\"")
+			.append(i)
+			.append("\" name=\"")
+			.append("Person_")
+			.append(i)
+			.append("\"/>");
+		}
+		sb.append("</Persons>");
+		PERSONS_RESPONSE = sb.toString();
+	}
+	
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         ServletHandler handler = new ServletHandler();
@@ -27,10 +46,7 @@ public class MinimalServer {
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             response.setContentType("text/plain");
             response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<?xml version=\"1.0\" encoding=\"utf-8\"?>"+ 
-            		"<Persons><Person number=\"00000915\" name=\"John Doe\"/>" +
-                    "<Person number=\"00000989\" name=\"Freddy Star\"/>" +
-                    "<Person number=\"00000922\" name=\"Hero Mero\"/></Persons>");
+            response.getWriter().println(PERSONS_RESPONSE);
         }
     }
 }
