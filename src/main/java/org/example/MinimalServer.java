ba77cce31a286bc7aa6ecc3642fa17754036ded0
Ivan Kravchenko <ikravchenko@debonw16002.stacobon.dd13.mydorma.com>
2014-07-22 16:06:48 +0200

    Added permissions impl

--- src/main/java/org/example/MinimalServer.java
+++ src/main/java/org/example/MinimalServer.java
@@ -13,6 +13,8 @@ public class MinimalServer {
  
 	private static final String PERSONS_RESPONSE;
 	private static final String DOORS_RESPONSE;
+	private static final String PERMISSIONS_RESPONSE;
+	
 	static {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?><Persons>");
@@ -27,7 +29,9 @@ public class MinimalServer {
 		}
 		sb.append("</Persons>");
 		PERSONS_RESPONSE = sb.toString();
-		
+	}
+	
+	static {
 		StringBuilder doorsSB = new StringBuilder();
 		doorsSB.append("<?xml version=\"1.0\" encoding=\"utf-8\"?><Doors>");
 		for (int i = 1; i <= 100; i++) {
@@ -43,11 +47,29 @@ public class MinimalServer {
 		DOORS_RESPONSE = doorsSB.toString();
 	}
 	
+	static {
+		StringBuilder permissionsSB = new StringBuilder();
+		permissionsSB.append("<?xml version=\"1.0\" encoding=\"utf-8\"?><Permissions>");
+		for (int i = 1; i <= 100; i+=9) {
+			for (int j = 1; j <= 100; j+=3) {
+				permissionsSB
+				.append("<Permission doorNumber=\"")
+				.append(i)
+				.append("\" personNumber=\"")
+				.append(j)
+				.append("\"/>");
+			}
+		}
+		permissionsSB.append("</Permissions>");
+		PERMISSIONS_RESPONSE = permissionsSB.toString();
+	}
+	
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(PersonsServlet.class, "/persons");
         handler.addServletWithMapping(DoorsServlet.class, "/doors");
+        handler.addServletWithMapping(PermissionsServlet.class, "/permissions");
         server.setHandler(handler);    
         server.start();
         server.join();
@@ -72,6 +94,17 @@ public class MinimalServer {
             response.setContentType("text/plain");
             response.setStatus(HttpServletResponse.SC_OK);
             response.getWriter().println(DOORS_RESPONSE);
+        }
+    }
+    
+    @SuppressWarnings("serial")
+    public static class PermissionsServlet extends HttpServlet {
+ 
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/plain");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(PERMISSIONS_RESPONSE);
         }
     }
 }
